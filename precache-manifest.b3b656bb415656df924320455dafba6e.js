a8d4fb53ae47fee3f1694af787b22c2147544c12
Neelam Chaurasia <chaurasia.neelam@gmail.com>
2019-01-12 16:53:00 -0500

    Updates

---
+++ precache-manifest.b3b656bb415656df924320455dafba6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c4c0c8a9143462d4578ec021e4dbf51",
+    "url": "/WithdrawMoney-React-Redux/static/media/girl.1c4c0c8a.png"
+  },
+  {
+    "revision": "2c0178d8d3127203f42f",
+    "url": "/WithdrawMoney-React-Redux/static/js/runtime~main.2c0178d8.js"
+  },
+  {
+    "revision": "e44312bbe23de3090bb6",
+    "url": "/WithdrawMoney-React-Redux/static/js/main.e44312bb.chunk.js"
+  },
+  {
+    "revision": "91acbdda4621fa5c2dcb",
+    "url": "/WithdrawMoney-React-Redux/static/js/1.91acbdda.chunk.js"
+  },
+  {
+    "revision": "e44312bbe23de3090bb6",
+    "url": "/WithdrawMoney-React-Redux/static/css/main.d8d76133.chunk.css"
+  },
+  {
+    "revision": "91acbdda4621fa5c2dcb",
+    "url": "/WithdrawMoney-React-Redux/static/css/1.b3e07de7.chunk.css"
+  },
+  {
+    "revision": "78c024cd07e911bb2c842411e5c8682b",
+    "url": "/WithdrawMoney-React-Redux/index.html"
+  }
+];
